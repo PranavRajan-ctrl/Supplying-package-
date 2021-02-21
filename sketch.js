1246b7bf8ab278d478ef4e2a86002ee92e584295
Pranav Rajan <75842883+PranavRajan-ctrl@users.noreply.github.com>
2021-02-21 21:41:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -81,16 +81,15 @@ function draw() {
  
 }
 
-function keyPressed{
-	if (keyCode===LEFT_ARROW){
-		helicopterSprite.x=helicopterSprite.x-20
-		translation{x:-20,y=0}
-		Matter.Body.translate(packageBody,translation)
-	}else if(keyCode===RIGHT_ARROW){
-		helicopterSprite.x=helicopterSprite.x+20
-		translation{x:-20,y=0}
-		Matter.Body.translate(packageBody,translation)
-	}else if (keyCode===DOWN_ARROW){
-              Matter.Body.setStacic(package,false)
-	}
-}
+function keyPressed() { 
+	if (keyCode === LEFT_ARROW) { 
+		helicopterSprite.x=helicopterSprite.x-20;
+		 translation={x:-20,y:0} Matter.Body.translate(packageBody, translation) }
+
+		  else if (keyCode === RIGHT_ARROW) { 
+			  helicopterSprite.x=helicopterSprite.x+20;
+			   translation={x:20,y:0} Matter.Body.translate(packageBody, translation) }
+			   
+			    else if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false);
+				 } 
+				}
